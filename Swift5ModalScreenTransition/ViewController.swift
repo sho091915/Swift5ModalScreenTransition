e8efda670e39ba1099f86c74472c9faf8d33df10
sho091915 <c0115162b3@edu.teu.ac.jp>
2020-02-02 00:45:04 +0900

    first commit

---
+++ Swift5ModalScreenTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5ModalScreenTransition
+//
+//  Created by 佐藤翔馬 on 2020/02/01.
+//  Copyright © 2020 Shoma Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
